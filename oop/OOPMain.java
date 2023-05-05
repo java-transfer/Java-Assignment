10c6076270834883ee58f5f86c652358a63344c9
Jesse Moses <jessemoses71@gmail.com>
2023-05-05 14:01:58 +0100

    Cleared refference commented code.

--- oop/OOPMain.java
+++ oop/OOPMain.java
@@ -38,19 +38,5 @@ public class OOPMain {
             System.out.println("Something went wrong.");
         }
 
-        // Geometry geometry = new Geometry();
-        // geometry.accessingPi();
-
-        // Scanner input = new Scanner(System.in);
-        // try{
-        // float length = input.nextFloat();
-        // float breadth = input.nextFloat();
-
-        // System.out.println(geometry.areaOfTriangle(breadth, length));
-        // }catch (InputMismatchException ime){
-        // System.out.println("Enter a valid number");
-        // }catch (Exception e){
-        // System.out.println("Something went wrong");
-        // }
     }
 }
--- oop/Physics.java
+++ oop/Physics.java
@@ -4,18 +4,6 @@ public class Physics extends Functions {
 
     void getKineticEnergy(float mass, float velocity) {
         System.out.print(divide(mass, 2) * powTwo(velocity));
-        System.out.print(" Joules");
+        System.out.print(" m/s squared");
     }
-
-    // float areaOfSquare(float length) {
-    // return powTwo(length);
-    // }
-
-    // float areaOfTriangle(float breadth, float length) {
-    // return divide(1, 2) * multiply(breadth, length);
-    // }
-
-    // float areaOfRectangle(float length, float breadth) {
-    // return multiply(length, breadth);
-    // }
 }
